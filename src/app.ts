aadf717a7fa3d04c9b9b3483bace5bc5ef223219
Harrypoggers25 <mfa.g4e@gmail.com>
2026-04-06 11:37:26 +0800

    Added App module

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('hello world');
+// MODULES
+import App from "@harrypoggers25/app-express";
+
+// CONFIGS
+import env from "./configs/env.config";
+// import router from "./routers";
+import { db } from "./configs/db.config";
+
+App.listen({
+    port: env.PORT,
+    version: '1.0.0 build 1',
+    cors: [env.ORIGIN_URL],
+    beforeListen: async (app) => {
+        // app.use('/', router);
+    },
+    callback: async (app, server) => {
+        await db.sync({ alter: false });
+    }
+});

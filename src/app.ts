7d8838f99c0b092319204ca8384aa661f605275c
Harrypoggers25 <mfa.g4e@gmail.com>
2026-04-07 12:40:34 +0800

    Build 2

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { db } from "./configs/db.config";
 
 App.listen({
     port: env.PORT,
-    version: '1.0.0 build 1',
+    version: '1.0.0 build 2',
     cors: [env.ORIGIN_URL],
     beforeListen: async (app) => {
         app.use('/', router);

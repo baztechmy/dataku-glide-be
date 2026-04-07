4d18baf0dc13d46624df056b6b11b257190ffee9
Harrypoggers25 <mfa.g4e@gmail.com>
2026-04-07 16:29:30 +0800

    Implemented roles and account owner access control to users router

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -11,17 +11,18 @@ import {
 } from '../controllers/user.controller';
 
 // MIDDLEWARES
-// import Authorize from '../middlewares/authorization.middleware';
+import Authorize from '../middlewares/authorization.middleware';
+import AccessControl from '../middlewares/access-control.middleware';
 
 const userRouter = Router();
-// userRouter.use(Authorize.accesstoken);
+userRouter.use(Authorize.accesstoken);
 
 userRouter.route('/')
-    .post(createUserHandler)
-    .get(findAllUserHandler);
+    .post(AccessControl.roles(['superadmin', 'admin']), createUserHandler)
+    .get(AccessControl.roles(['superadmin', 'admin']), findAllUserHandler);
 userRouter.route('/:user_id')
-    .get(findUserHandler)
-    .patch(updateUserHandler)
-    .delete(deleteUserHandler);
+    .get(AccessControl.roles(['superadmin', 'admin']), findUserHandler)
+    .patch(AccessControl.rolesOrAccountOwner(['superadmin', 'admin']), updateUserHandler)
+    .delete(AccessControl.roles(['superadmin', 'admin']), deleteUserHandler);
 
 export default userRouter;

4a9fbc9de11e68f2b7aaa13af781318dd3b6b1ad
Harrypoggers25 <mfa.g4e@gmail.com>
2026-04-09 14:43:13 +0800

    Updated backup script

--- src/scripts/backup.script.ts
+++ src/scripts/backup.script.ts
@@ -3,21 +3,21 @@ import { User, UserSecret, UserActivityLog, MonitorTank, MonitorTankLog } from "
 
 (async () => {
     const append = true;
-    const path = './database/dataku-glide-v2.sql';
+    const path = './database/dataku-glide-v3.sql';
 
-    const users = await User.backup(path, { orderBy: { user_id: 'ASC' } });
+    const users = await User.backup(path, { fixSequence: 'user_id', orderBy: { user_id: 'ASC' } });
     if (!users) return;
 
     const userSecrets = await UserSecret.backup(path, { orderBy: { user_id: 'ASC' }, append });
     if (!userSecrets) return;
 
-    const userActivityLogs = await UserActivityLog.backup(path, { orderBy: { ual_id: 'ASC' }, append });
+    const userActivityLogs = await UserActivityLog.backup(path, { fixSequence: 'ual_id', orderBy: { ual_id: 'ASC' }, append });
     if (!userActivityLogs) return;
 
-    const monitorTank = await MonitorTank.backup(path, { orderBy: { mt_id: 'ASC' }, append });
+    const monitorTank = await MonitorTank.backup(path, { fixSequence: 'mt_id', orderBy: { mt_id: 'ASC' }, append });
     if (!monitorTank) return;
 
-    const monitorTankLog = await MonitorTankLog.backup(path, { orderBy: { mtl_id: 'ASC' }, append });
+    const monitorTankLog = await MonitorTankLog.backup(path, { fixSequence: 'mtl_id', orderBy: { mtl_id: 'ASC' }, append });
     if (!monitorTankLog) return;
 
     console.log(ch.green('SCRIPT:'), 'All db data has been', ch.green('successfully'), 'backed up');
